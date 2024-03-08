a0f61cc887673c73621c81fa5e7c999ed50b3725
Nickolay Bukreyev <buknik95@yandex.ru>
2024-03-08 23:07:26 +0700

    Overhaul the identifier-conversion function

--- src/sourcegens/identStyles.nim
+++ src/sourcegens/identStyles.nim
@@ -6,40 +6,56 @@ type
   IdentStyle* = object
     initial*, wordInitial*, rest*: LetterCase
     wordSep*: string
+    alphabet*: set[char]
+
+func initIdentStyle*(
+  initial = lcLower;
+  wordInitial = lcLower;
+  rest = lcLower;
+  wordSep = "";
+  alphabet = {'A' .. 'Z', 'a' .. 'z', '0' .. '9'};
+): IdentStyle {.inline.} =
+  IdentStyle(
+    initial: initial, wordInitial: wordInitial, rest: rest, wordSep: wordSep, alphabet: alphabet,
+  )
 
 func changeCase(c: char; lc: LetterCase): char =
   if lc == lcLower: c.toLowerAscii else: c.toUpperAscii
 
 func convertStyle*(s: openArray[char]; style: IdentStyle): string =
-  var i = 0
   let n = s.len
+  if n == 0: return
+
   result = newStringOfCap n # Can reallocate later.
+  var i = ord s[0] not_in style.alphabet
+  if i != 0:
+    if style.wordSep.len != 0:
+      result.add style.wordSep
+    else:
+      result.add '_'
+    while (if i == n: return; s[i] not_in style.alphabet):
+      i += 1
+  result.add s[i].changeCase style.initial
 
-  let leadingSep = if style.wordSep != "-": '_' else: '-'
-  while i != n and s[i] in {'_', '-'}:
-    result.add leadingSep
-
-  if i != n:
-    result.add s[i].changeCase style.initial
-
-    var midWord = true
-    var needSep = true
-    for i in i + 1 ..< n:
-      let c = s[i]
-      if c in {'A' .. 'Z', '_', '-'}:
-        midWord = false
-        if c not_in {'A' .. 'Z'}:
-          needSep = false
+  var midWord = true
+  var needSep = true
+  for i in i + 1 ..< n:
+    let c = s[i]
+    if c not_in 'a' .. 'z':
+      midWord = false
+      if c not_in style.alphabet:
+        if needSep:
           result.add style.wordSep
-          continue
+          needSep = false
+        continue
 
-      result.add s[i].changeCase do:
-        if midWord:
-          style.rest
+    result.add s[i].changeCase do:
+      if midWord:
+        style.rest
+      else:
+        midWord = true
+        if needSep:
+          result.add style.wordSep
         else:
-          midWord = true
-          if needSep:
-            result.add style.wordSep
-          else:
-            needSep = true
-          style.wordInitial
+          needSep = true
+        style.wordInitial
--- tests/test.nim
+++ tests/test.nim
@@ -1,5 +1,60 @@
 import unittest
 import sourcegens
 
-test "nop":
-  discard
+suite "`identStyle`":
+  test "Converts to camel case":
+    const st = initIdentStyle(wordInitial = lcUpper)
+    check convertStyle("", st) == ""
+    check convertStyle("abc", st) == "abc"
+    check convertStyle("A_b_c", st) == "aBC"
+    check convertStyle("-a-b-c", st) == "_aBC"
+    check convertStyle("abCDef", st) == "abCDef"
+    check convertStyle("ab-cd-ef", st) == "abCdEf"
+    check convertStyle("$_ab_$cd", st) == "_abCd"
+    check convertStyle("r2d2", st) == "r2d2"
+    check convertStyle("a.1/b.2", st) == "a1B2"
+    check convertStyle("0a", st) == "0a"
+    check convertStyle("0A", st) == "0A"
+
+  test "Converts to Pascal case":
+    const st = initIdentStyle(initial = lcUpper, wordInitial = lcUpper)
+    check convertStyle("", st) == ""
+    check convertStyle("abc", st) == "Abc"
+    check convertStyle("A_b_c", st) == "ABC"
+    check convertStyle("-a-b-c", st) == "_ABC"
+    check convertStyle("abCDef", st) == "AbCDef"
+    check convertStyle("ab-cd-ef", st) == "AbCdEf"
+    check convertStyle("$_ab_$cd", st) == "_AbCd"
+    check convertStyle("r2d2", st) == "R2d2"
+    check convertStyle("a.1/b.2", st) == "A1B2"
+    check convertStyle("0a", st) == "0a"
+    check convertStyle("0A", st) == "0A"
+
+  test "Converts to snake case":
+    const st = initIdentStyle(wordSep = "_")
+    check convertStyle("", st) == ""
+    check convertStyle("abc", st) == "abc"
+    check convertStyle("A_b_c", st) == "a_b_c"
+    check convertStyle("-a-b-c", st) == "_a_b_c"
+    check convertStyle("abCDef", st) == "ab_c_def"
+    check convertStyle("ab-cd-ef", st) == "ab_cd_ef"
+    check convertStyle("$_ab_$cd", st) == "_ab_cd"
+    check convertStyle("r2d2", st) == "r_2d_2"
+    check convertStyle("a.1/b.2", st) == "a_1_b_2"
+    check convertStyle("0a", st) == "0a"
+    check convertStyle("0A", st) == "0_a"
+
+  test "Converts to camel case with dollars":
+    const st =
+      initIdentStyle(wordInitial = lcUpper, alphabet = {'A' .. 'Z', 'a' .. 'z', '0' .. '9', '$'})
+    check convertStyle("", st) == ""
+    check convertStyle("abc", st) == "abc"
+    check convertStyle("A_b_c", st) == "aBC"
+    check convertStyle("-a-b-c", st) == "_aBC"
+    check convertStyle("abCDef", st) == "abCDef"
+    check convertStyle("ab-cd-ef", st) == "abCdEf"
+    check convertStyle("$_ab_$cd", st) == "$Ab$cd"
+    check convertStyle("r2d2", st) == "r2d2"
+    check convertStyle("a.1/b.2", st) == "a1B2"
+    check convertStyle("0a", st) == "0a"
+    check convertStyle("0A", st) == "0A"
